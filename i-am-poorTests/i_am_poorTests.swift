05b75259fdbdd5149950eb3ce04f6933a78277c0
Rodrigo Abreu <rodrigo.abreu@unicred.com.br>
2025-09-24 07:21:34 -0300

    chore: initial commit

---
+++ i-am-poorTests/i_am_poorTests.swift
@@ -0,0 +1,17 @@
+//
+//  i_am_poorTests.swift
+//  i-am-poorTests
+//
+//  Created by Rodrigo Abreu on 9/24/25.
+//
+
+import Testing
+@testable import i_am_poor
+
+struct i_am_poorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
